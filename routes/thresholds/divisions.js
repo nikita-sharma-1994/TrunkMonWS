c6871aee013890c1d8f05160e1f64e8aa72dc568
nikita-sharma-1994 <nikita.sharma@idt.net>
2016-07-19 16:23:02 -0400

    As on 7/19/2016

--- routes/thresholds/divisions.js
+++ routes/thresholds/divisions.js
@@ -2,7 +2,10 @@ var express = require('express');
 var oracledb = require('oracledb');
 var router = express.Router();
 
-/* GET divisions. */
+//set oracledb format to get output in object format
+oracledb.outFormat = oracledb.OBJECT;
+
+// GET divisions
 router.get('/', function(req, res) {
   oracledb.getConnection(
     {user : "trunkmon_app",
@@ -10,11 +13,11 @@ router.get('/', function(req, res) {
      connectString : "10.227.1.96:1521/rdev"
     },
     function(err,connection) {
-       if (err) {res.status(500).json({error : "couldn't connect to oracle databse"}); return;}
+       if (err) {res.status(err.status || 500).json({error : "couldn't connect to oracle databse", type : err.message}); return;}
        connection.execute(
          "SELECT DIVISION_CODE, DIVISION_NAME FROM TRUNKMON.DIVISION_LOOKUP",
          function(err,result) {
-            if (err) {res.status(500).json({error : "couldn't read from oracle databse"}); return;}
+            if (err) {res.status(err.status || 500).json({error : "couldn't read from oracle databse", type : err.message}); return;}
             res.status(200).json(result.rows);     
        }); 
   }); 
--- routes/violations/freshness.js
+++ routes/violations/freshness.js
@@ -2,28 +2,32 @@ var express = require('express');
 var oracledb = require('oracledb');
 var router = express.Router();
 
-/* check freshness */
+//set oracledb format to get output in array format
+oracledb.outFormat = oracledb.ARRAY;
+
+// Check freshness
 router.get('/', function(req, res) {
   if(typeof req.param('datetime')  === 'undefined' || typeof req.param('datetime')  === 'null'){
-  res.status(500).json({error : "requires parameter datetime"}); return;}
+  res.status(500).json({error : "Missing Parameter : datetime"}); return;}
   oracledb.getConnection(
     {user : "trunkmon_app",
      password : "trunkmon_app",
      connectString : "10.227.1.96:1521/rdev"
     },
     function(err,connection) {
-       if (err) {res.status(500).json({error : "couldn't connect to oracle databse"}); return;}
+       if (err) {res.status(err.status || 500).json({error : "couldn't connect to oracle databse", type : err.message}); return;}
        var queryString = "SELECT CREATE_DATE FROM TRUNKMON.GEN_SESSION";
        connection.execute(queryString,
          function(err,result) {
-            if (err) {res.status(500).json({error : "couldn't read from oracle databse"}); return;}
-            /*res.status(200).json({datetime1 : new Date(result.rows[0]), datetime2 : new Date(result.rows[1]), datetime3 : new Date(req.param('datetime')), length : result.rows.length});}*/
+            if (err) {res.status(err.status || 500).json({error : "couldn't read from oracle databse", type : err.message}); return;}
+            /*res.status(200).json({datetime1 : new Date(result.rows[0]), datetime2 : new Date(result.rows[1]), datetime3 : new Date(req.param('datetime')), length : result.rows.length});*/
+          
             for(var i = 0; i < result.rows.length; i++){
               if(new Date(result.rows[i]) > new Date(req.param('datetime'))) {
                  res.status(200).json({isFresh : "false", changeTime : result.rows[i]}); return;
               } 
             }
-            res.status(200).json({isFresh : "true"});   
+            res.status(200).json({isFresh : "true"});  
        }); 
   }); 
 });
